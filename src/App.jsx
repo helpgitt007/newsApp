a2c0fbb4ae634d815b12e32925c8ea49ae0cd81f
helpgitt007 <anas@invis.in>
2024-06-26 19:54:59 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1>Welcome to the land of react.js</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
